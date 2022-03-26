a94051c53d306aeb31a827c4bf9eefd5724d4488
Rojberr <rojberr@outlook.com>
2022-03-26 21:39:35 +0100

    Init commit

---
+++ src/main/java/dev/drzymala/replacer/ReplacerApplication.java
@@ -0,0 +1,13 @@
+package dev.drzymala.replacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplacerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drzymala/replacer/ReplacerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drzymala.replacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
