43e915f1a256312b99c6208985daee449ce6a4b3
Rojberr <rojberr@outlook.com>
2022-04-13 17:26:54 +0200

    feat(): Add extends

--- src/main/java/dev/drzymala/replacer/ReplacerSecurityConfiguration.java
+++ src/main/java/dev/drzymala/replacer/ReplacerSecurityConfiguration.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @AllArgsConstructor
-public class ReplacerSecurityConfiguration {
+public class ReplacerSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
